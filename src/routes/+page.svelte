38d8d932340a6adeeb74568861114547eecf6f9c
Raj Shekhar <channiecx954@gmail.com>
2023-01-09 19:39:10 +0530

    Style todo item

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
-	let tasks = [{ done: false, name: 'Clean my room' }];
+	let tasks = [{ done: false, name: '' }];
+	let newTask = '';
 
 	import Todo from './components/Todo.svelte';
 
@@ -9,18 +10,41 @@
 	function handleComplete(event) {
 		alert(event.detail.task.name);
 	}
+
+	function addTodo() {
+		tasks = [...tasks, { done: false, name: newTask }];
+		newTask = '';
+	}
 </script>
 
-<main class="m-6">
+<main class="my-6 w-screen h-screen flex flex-col">
 	<header class="w-full text-center">
-		<h1 class="text-2xl">Manage your goals hassle-free</h1>
+		<h1 class="text-2xl">Make your life organised.</h1>
 	</header>
 
-	<div class="w-3/4 flex justify-center">
-		<ul class="my-4">
-			{#each tasks as task (task.name)}
-				<Todo {task} on:complete={handleComplete} />
-			{/each}
-		</ul>
-	</div>
+	<h2 class="text-center text-xl my-6">Your tasks</h2>
+
+	<section class="w-full flex justify-center">
+		<div class="w-3/4 md:w-1/2">
+			<form on:submit|preventDefault={addTodo}>
+				<div class="flex gap-2 my-4">
+					<input
+						type="text"
+						placeholder="Wish to track a chore? Add it here"
+						class="w-full h-10 ring-2 ring-indigo-400 focus:ring-indigo-700 hover:ring-indigo-500 outline-none rounded-md px-4"
+						bind:value={newTask}
+					/>
+					<button class="rounded bg-indigo-600 h-10 w-20 text-white"> Add </button>
+				</div>
+			</form>
+
+			<div class="my-6">
+				{#each tasks as task (task.name)}
+					{#if task.name.length > 0}
+						<Todo {task} />
+					{/if}
+				{/each}
+			</div>
+		</div>
+	</section>
 </main>
--- src/routes/components/Todo.svelte
+++ src/routes/components/Todo.svelte
@@ -1,15 +1,41 @@
 <script>
 	export let task = { done: false, name: '' };
-
-	import { createEventDispatcher } from 'svelte';
-	const dispatch = createEventDispatcher();
-
-	function handleClick() {
-		dispatch('complete', {
-			task
-		});
-	}
 </script>
 
-<input id={task.name} type="checkbox" value={task.name} on:click|once={(e) => handleClick()} />
-<label for={task.name}>{task.name}</label>
+<div
+	class="h-16 my-4 flex justify-between items-center px-4 rounded-xl shadow-lg border-slate-100 bg-slate-50 hover:bg-slate-200 duration-100 hover:border-0 text-slate-900"
+>
+	<div id="check_and_taskname">
+		{task.name}
+	</div>
+	<div id="action_items" class="flex gap-4">
+		<button class="text-lime-600 hover:scale-150 duration-300">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm13.36-1.814a.75.75 0 10-1.22-.872l-3.236 4.53L9.53 12.22a.75.75 0 00-1.06 1.06l2.25 2.25a.75.75 0 001.14-.094l3.75-5.25z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</button>
+		<button class="text-red-600 hover:scale-150 duration-300">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				viewBox="0 0 24 24"
+				fill="currentColor"
+				class="w-6 h-6"
+			>
+				<path
+					fill-rule="evenodd"
+					d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</button>
+	</div>
+</div>
