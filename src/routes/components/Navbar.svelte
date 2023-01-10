b2207173bc18b65273a7c77656ae71992a2b9a1c
Raj Shekhar <channiecx954@gmail.com>
2023-01-11 00:36:42 +0530

    Add null check

--- src/routes/components/Navbar.svelte
+++ src/routes/components/Navbar.svelte
@@ -7,7 +7,7 @@
 	<div class="h-full w-36 overflow-clip flex items-center">
 		<img class="overflow-clip" src="brandblack.png" alt="Do it brand logo" />
 	</div>
-	{#if $user.picture}
+	{#if $user?.picture}
 		<div class="h-full w-10 rounded-full flex items-center">
 			<img class="rounded-full" src={$user.picture} alt={$user.nickname} />
 		</div>
